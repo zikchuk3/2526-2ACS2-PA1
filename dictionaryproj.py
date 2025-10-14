8b15c1d90dcbfa41e5bc416f7deb4de32b7edf13
Zikora C <zchukwumah17@gmail.com>
2025-10-14 09:39:27 -0400

    Update dictionaryproj.py

--- dictionaryproj.py
+++ dictionaryproj.py
@@ -1,69 +1,105 @@
+import random
+
+#mods
 mod1 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod2 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod3 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod4 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod5 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod6 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 mod7 = {
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
+    "A Block": "",
+    "B Block": "",
+    "C Block": "",
+    "D Block": "",
+    "E Block": "Nothing"
 }
 
+#lists of classes d blocks and e blocks
 classes_list_english = ["English I", "English II", "English III", "English IV"]
 classes_list_math = ["Geometry", "Algebra I", "Algebra II", "Pre-Calculus", "Calculus AB", "Calculus BC", "Statistics", "Linear Algebra", "Differiential Equations" ]
-classes_list_history = ["Globral I", "Global II", "American History", "History Research"]
+classes_list_history = ["Globral I", "Global II", "American History", "History Capstone Seminar", "Research Seminar"]
 classes_list_science = ["Biology", "Chemistry", "Physics"]
-classes_list_languages = ["Spanish I", "Spanish II", ]
-electives = ["CS1", "CS2", "CS3", "Digital Art & Animation", "Drawing", "Painting", "Art Seminar", "Computer Science Seminar", "Robotics", "Public Speaking", "Fibers & Fashion", "Photography", "Sports Psychology", "Ceramics" ]
-d_blocksF = ["Feild Hockey", "Swimming for Conditioning", "Cross Country", "Soccer", "Tennis", "Volleyball", "Yearbook", "Advanced Fitness", "Stagecraft"]
-d_blocksW = ["Basektball", "Rock Climbing", "Swim", "Model UN", "Advanced Fitness", "Yearbook", "Stagecraft"]
+#co_curric = ["Sophomore Co-Curric", "Junior Co-Corric", "Senior Co-Curric"]
+classes_list_languages = ["Spanish I", "Spanish II", "Spanish III", "Spanish IV", "French I", "Frecnh II", "French III", "French IV", "Latin I", "Latin II", "Latin III", "Latin IV"]
+electives = ["Free", "Admissions", "Library Helper", "CS1", "CS2", "CS3", "Digital Art & Animation", "Drawing", "Painting", "Art Seminar", "Computer Science Seminar", "Robotics", "Public Speaking", "Fibers & Fashion", "Photography", "Sports Psychology", "Ceramics" ]
+d_blocksF = ["Feild Hockey", "Swimming for Conditioning", "Cross Country", "Soccer", "Tennis", "Volleyball", "Yearbook", "Advanced Fitness", "Stagecraft", "Fall Play"]
+d_blocksW = ["Basektball", "Rock Climbing", "Swim", "Model UN", "Advanced Fitness", "Yearbook", "Stagecraft", "Winter Musical"]
 d_blocksS = ["Track & Feild", "Tennis", "Softball", "Lacrosse", "Advanced Fitness", "Nature Walks"]
-e_blocks = ["Chorus", ]
+e_blocks = ["Chorus", "Orchestra" ]
+
+#random choice of classes/dblocks for schedules 
+#will just make electives and e blocks add-able
+english = random.choice(classes_list_english)
+math = random.choice(classes_list_math)
+history = random.choice(classes_list_history)
+science = random.choice(classes_list_science)
+lang = random.choice(classes_list_languages)
+classes = [english, math, history, science, lang]
+
+fall = random.choice(d_blocksF)
+winter = random.choice(d_blocksW)
+spring = random.choice(d_blocksS)
+
+#making the schedule
+def make_schedule():
+    for blocks in mod1:
+        if blocks in ["A Block", "B Block", "C Block"]:
+            return random.choice(classes)
 
 #add class
 def add_class(schedule):
     print("Here is your current schedule: ")
+    for block in mod1:
+        print(f"{block}: {mod1[block]}")
     for block in mod2:
         print(f"{block}: {mod2[block]}")
+    for block in mod3:
+        print(f"{block}: {mod3[block]}")
+    for block in mod4:
+        print(f"{block}: {mod4[block]}")
+    for block in mod5:
+        print(f"{block}: {mod5[block]}")
+    for block in mod6:
+        print(f"{block}: {mod6[block]}")
+    for block in mod7:
+        print(f"{block}: {mod7[block]}")
 
     #user says what they want to add
     block = input("Enter the block you want to add the class in: ")
@@ -78,8 +114,20 @@ def add_class(schedule):
 #change class
 def change_class(schedule):
     print("Here is your current schedule: ")
+    for block in mod1:
+        print(f"{block}: {mod1[block]}")
     for block in mod2:
         print(f"{block}: {mod2[block]}")
+    for block in mod3:
+        print(f"{block}: {mod3[block]}")
+    for block in mod4:
+        print(f"{block}: {mod4[block]}")
+    for block in mod5:
+        print(f"{block}: {mod5[block]}")
+    for block in mod6:
+        print(f"{block}: {mod6[block]}")
+    for block in mod7:
+        print(f"{block}: {mod7[block]}")
 
     #user says what they want to add
     block = input("Enter the block you want to change: ")
@@ -103,8 +151,20 @@ def change_class(schedule):
 #drop class
 def drop_class(schedule):
     print("Here is your current schedule: ")
+    for block in mod1:
+        print(f"{block}: {mod1[block]}")
     for block in mod2:
         print(f"{block}: {mod2[block]}")
+    for block in mod3:
+        print(f"{block}: {mod3[block]}")
+    for block in mod4:
+        print(f"{block}: {mod4[block]}")
+    for block in mod5:
+        print(f"{block}: {mod5[block]}")
+    for block in mod6:
+        print(f"{block}: {mod6[block]}")
+    for block in mod7:
+        print(f"{block}: {mod7[block]}")
 
     for block in mod2:
         block = input("What block would you like to drop a class from? ")
@@ -116,6 +176,7 @@ def drop_class(schedule):
     return schedule
 
 def main():
+    #welcome in the offic 
     print("Welcome to Mrs. Caroll's office.")
     user_input = input("Would you like to add, change, or drop a class? ").lower()
     valid_actions = ["add", "drop", "change"]
@@ -123,13 +184,25 @@ def main():
         print("Error. Please pick 'add', 'change' or 'drop'.")
         user_input = input("Would you like to add, change, or drop a class? ").lower()
     if user_input == "add":
-        add_class(mod2)
+        add_class()
     elif user_input == "change":
-        change_class(mod2)
+        change_class()
     elif user_input == "drop":
-        drop_class(mod2)
+        drop_class()
 
     print("Here is your final schedule: ")
+    for block in mod1:
+        print(f"{block}: {mod1[block]}")
     for block in mod2:
         print(f"{block}: {mod2[block]}")
+    for block in mod3:
+        print(f"{block}: {mod3[block]}")
+    for block in mod4:
+        print(f"{block}: {mod4[block]}")
+    for block in mod5:
+        print(f"{block}: {mod5[block]}")
+    for block in mod6:
+        print(f"{block}: {mod6[block]}")
+    for block in mod7:
+        print(f"{block}: {mod7[block]}")
 main()
