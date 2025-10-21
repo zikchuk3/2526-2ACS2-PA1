c14f01c47f867cc55fd4d5c566ba04dc608e4229
Zikora C <zchukwumah17@gmail.com>
2025-10-21 09:05:47 -0400

    Update oct20_files.py
    explored the read method and readline methods for files

--- oct20_files.py
+++ oct20_files.py
@@ -1,7 +1,18 @@
+'''
+
 my_schedule = open("schedule.txt")
 
 #opens file and says it can acess it
 print(my_schedule)
 
 #allows us to see/read the file
-#print(my_schedule.read())
+print(my_schedule.read()) # prints whole thing
+
+print(my_schedule.read(5)) #prints number of characters in ()
+
+print(my_schedule.readline()) # to print specific lines
+
+my_schedule.readline() # it read a lime w/o printing, skips to next line that you print
+print(my_schedule.read(3))
+
+'''
