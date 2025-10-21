2b962d48ad42da7f7f9cf244c6f0320352eee900
Zikora C <zchukwumah17@gmail.com>
2025-10-21 09:50:56 -0400

    modes
    modes of ways to deal with files

--- oct21.py
+++ oct21.py
@@ -1,7 +1,22 @@
-my_schedule = open("schedule.txt")
+my_schedule = open("schedule.txt") #by default opens in read mode, can open in write or append mode
+
+#my_schedule = open("schedule.txt", "w") #write mode
+#my_schedule.write("Changing content!") #overwrites all files contents
+
+my_schedule = open("schedule.txt", "a") # append mode
+my_schedule.write("\nLOOK AT NEW CONTENT") #adds to end of the file contents
+
+'''
+To create a new file in Python, just open one that doesnt exist in an edit mode
+'''
+#new_file = open("new_file.txt", "x") #just creates new file
+#new_file.write("HI")
+
+new_file = open("new_file.txt", "a") #can create file w/ append and write to
+new_file.write("yo")
+
 
 #write a while loop that looks for mod 2
-
 '''
 ps = True
 print(my_schedule)
@@ -19,6 +34,7 @@ while ps:
 
 '''
 
+'''
 #better way of while loop
 line = my_schedule.readline()
 
@@ -29,3 +45,4 @@ print(line) #give it a mod 2 label
 #then write a for loop that prints out mod 2 schedule blocks
 for i in range(3):
     print(my_schedule.readline())
+'''
